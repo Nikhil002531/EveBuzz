18113cca26ba0193c3095d9aef2bd6079bde1fd5
Raunak Rose <raunakrose15@gmail.com>
2025-05-25 21:12:46 +0530

    login ui added

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import EveBuzzThreeJS from "@/components/evebuzz-threejs-3d";
+
+export default function LoginPage() {
+
+  return (
+    <div>
+      <EveBuzzThreeJS />
+    </div>
+  )
+}
