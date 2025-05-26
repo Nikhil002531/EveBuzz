c0876c6167727321bc9a39e1c65dc897b5834837
Raunak Rose <raunakrose15@gmail.com>
2025-05-26 10:16:30 +0530

    changes to models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,2 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
+class Event(models.Model):
+    EVENT_TYPE_CHOICES = [
+        ("hackathon", "Hackathon"),
+        ("cultural", "Cultural"),
+        ("sports", "Sports"),
+        ("competition", "Competition"),
+        ("workshop", "Workshop"),
+        ("seminar", "Seminar"),
+        ("others", "Others"),
+    ]
+
+    title = models.CharField(max_length=100, default="")
+    type = models.CharField(
+        max_length=50, choices=EVENT_TYPE_CHOICES, default="hackathon"
+    )
+    other_type_name = models.CharField(
+        max_length=100, blank=True, null=True, help_text="Specify if type is 'Others'"
+    )
+    image = models.ImageField(
+        upload_to="events/images/", default="events/images/default.jpg"
+    )
+    description = models.TextField(default="")
+    minTeamParticipants = models.IntegerField(default=0)
+    maxTeamParticipants = models.IntegerField(default=1)
+    location = models.CharField(max_length=200, default="")
+    start_date = models.DateTimeField(default=None)
+    end_date = models.DateTimeField(default=None)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    organizer = models.CharField(max_length=100, default="")
+    contact_info = models.CharField(max_length=100, default="")
+    registrationLink = models.URLField(default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import viewsets
+from .models import Event
+from .serializers import EventSerializer
 
 # Create your views here.
 from django.http import JsonResponse
@@ -16,3 +21,8 @@ class CreatUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
